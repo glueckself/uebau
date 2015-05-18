f4eb11431dd942f2308c4f5f0ea31b4f579a52ae
Srdjan Markovic <e1025857@student.tuwien.ac.at>
2015-05-18 23:16:33 +0200

    more list test fixes

--- codea/tests/helper.h
+++ codea/tests/helper.h
@@ -39,14 +39,15 @@ typedef struct {
     l1=(list1);                                 \
     l2=(list2);                                 \
     for(i=0; l2[i].type != ELEM_END; i++) {     \
+        printf("checking list at %x\n", l1);    \
         if(!IS_LIST(l1)) {                      \
-            printf("not a list: %x", (long)l1); \
+            printf("not a list: %x\n", (long)l1); \
             return 0;                           \
         }                                       \
         l1=UNTAG_LIST(l1);                      \
         val=LIST_HEAD(l1);                      \
         if(val != l2[i].value) {                \
-            printf("value mismatch: %d/%d", val, l2[i].value); \
+            printf("value mismatch: %d/%d\n", val, l2[i].value); \
             return 0;                           \
         }                                       \
         l1=LIST_TAIL(l1);                       \
