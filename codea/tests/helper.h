450077393e4f3c9524dd5fc8cfd7200f9c8863e9
Srdjan Markovic <e1025857@student.tuwien.ac.at>
2015-05-18 22:31:38 +0200

    added list tests

--- codea/tests/helper.h
+++ codea/tests/helper.h
@@ -1,6 +1,7 @@
 typedef enum {
 	ELEM_NUM=0,
-	ELEM_LIST
+	ELEM_LIST,
+        ELEM_END
 } eListElemType;
 
 typedef struct {
@@ -26,8 +27,21 @@ typedef struct {
 #define IS_NUM(n) (((n) & 1) == 0)
 #define IS_LIST(n) (((n) & 3) == 1)
 #define IS_FUN(n) (((n) & 3) == 3)
+
+#define LIST_HEAD(l) ( *((long*)l) )
+#define LIST_TAIL(l) ( *( ((long*)l)+1 ) )
 	
-#define COMPARE_LIST(list1, list2) do { 	\
+#define LIST_EQUAL(list1, list2) do { 	\
+    int i;                                      \
+    void *l1, *l2;                              \
+    l1=(list1);                                 \
+    l2=(list2);                                 \
+    for(i=0; l2[i].type != ELEM_END; i++) {     \
+        if(LIST_HEAD(l1) != l2[i].value)        \
+            return 0;                           \
+        l1=LIST_TAIL(l1);                       \
+    }                                           \
+    return 1;                                   \
 } while(0);
 
 
